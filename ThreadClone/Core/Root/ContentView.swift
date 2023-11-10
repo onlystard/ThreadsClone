90c52c8a2d6de17552d5b824b0bec8cf098bd031
Pham Thien Tan <onlystard@gmail.com>
2023-11-10 14:11:29 +0700

    feat: update tab bả

---
+++ ThreadClone/Core/Root/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThreadClone
+//
+//  Created by Kay on 10/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
